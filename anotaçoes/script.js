93e2ab4f53247f4fe47860ef8418b470d533e6d7
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-05-10 09:52:48 -0300

    Uso do setAtributte e classList no JS

---
+++ anotaçoes/script.js
@@ -0,0 +1,8 @@
+let meuTitulo = document.querySelector('h1');
+
+// meuTitulo.setAttribute('style', 'font-size: 38px; background-color: purple');
+
+meuTitulo.classList.add('cor');
+meuTitulo.classList.add('tamanho');
+meuTitulo.classList.replace('tamanho', 'mudaTudo')
+console.log(meuTitulo);
