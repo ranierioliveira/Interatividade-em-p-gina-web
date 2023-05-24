4b96ad68744eed8ed8e45ea1b2c6d0b68d41b063
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-05-23 22:07:04 -0300

    Anotações da aula do dia 23/05

---
+++ anotaçoes/eventoClick.js
@@ -0,0 +1,4 @@
+const enviarNotas = document.getElementById('calcular');
+const resultado = document.querySelector('.resultado');
+
+enviarNotas.addEventListener('click', () => resultado.innerHTML = 'Fui clicado');
--- anotaçoes/script.js
+++ anotaçoes/script.js
@@ -1,4 +1,18 @@
-const enviarNotas = document.getElementById('calcular');
-const resultado = document.querySelector('.resultado');
+const meuBody = document.querySelector('body');
+const botao = document.querySelector('button');
+window.addEventListener('load', carregar);
+botao.addEventListener('click', () => {
+    let itemLista = prompt('Qual item você quer inserir na sua lista?');
+    if(itemLista.length > 0){
+        let ul = document.querySelector('ul');
+        let li = document.querySelector('li.item-menu');
+        console.log(li);
+    } else {
+        return;
+    }
+} )
 
-enviarNotas.addEventListener('click', () => resultado.innerHTML = 'Fui clicado');
+
+function carregar(){
+    meuBody.style.backgroundColor = 'steelblue';
+}
