85c5d7aa6d47aadbca75b035381c71e36184886a
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-05-26 18:04:03 -0300

    Aprendendo sobre cloneNode

--- anotaçoes/eventoLoad.js
+++ anotaçoes/eventoLoad.js
@@ -3,15 +3,16 @@ const botao = document.querySelector('button');
 window.addEventListener('load', carregar);
 botao.addEventListener('click', () => {
     let itemLista = prompt('Qual item você quer inserir na sua lista?');
-    if(itemLista.length > 0){
+    if(itemLista != ''){
         let ul = document.querySelector('ul');
-        let li = document.querySelector('li.item-menu');
-        console.log(li);
+        let li = document.querySelector('li.item-menu').cloneNode();
+        li.textContent = itemLista;
+        // li.setAttribute('contenteditable', '');
+        ul.appendChild(li);
     } else {
         return;
     }
-} )
-
+} );
 
 function carregar(){
     meuBody.style.backgroundColor = 'steelblue';
